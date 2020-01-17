18ba9d71e605f44ecfbf3b8713316eb0f7620253
Dilan Tharaka <dilant@codegen.net>
2020-01-17 16:43:00 +0530

    component with optimization

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Random name generator
-
 https://www.npmjs.com/package/unique-names-generator
+
+# Hardware Acceleration
+https://dev.opera.com/articles/css-will-change-property/
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {animals, colors, Config, uniqueNamesGenerator} from 'unique-names-generator';
+import {Animal} from './animal';
 
 
 @Component({
@@ -9,7 +10,7 @@ import {animals, colors, Config, uniqueNamesGenerator} from 'unique-names-genera
 })
 export class AppComponent {
 
-  public animalNames: string[] = [];
+  public animalsArray: Animal[] = [];
   private customConfig: Config = {
     dictionaries: [colors, animals],
     separator: ' ',
@@ -17,13 +18,26 @@ export class AppComponent {
   };
 
   constructor() {
-    for (let i = 0; i < 15; i++) {
+    for (let i = 0; i < 13; i++) {
       this.addNewAnimal();
     }
   }
 
   addNewAnimal() {
-    this.animalNames.push(uniqueNamesGenerator(this.customConfig));
+
+    const tmpArray: Animal[] = [];
+    for (const animal of this.animalsArray) {
+      tmpArray.push(new Animal(animal.name));
+    }
+    const animalName: string = uniqueNamesGenerator(this.customConfig);
+
+    tmpArray.push(new Animal(animalName));
+    this.animalsArray = [];
+    this.animalsArray = tmpArray;
+  }
+
+  trackAnimal(animalInstance: Animal) {
+    return animalInstance.name;
   }
 
 }
